f3b289067301d2505f91ec62a3cc98113528580d
Azahara Ruiz <azrugar@gmail.com>
2023-03-23 19:14:25 +0100

    fix:run and pass the test

--- src/components/button/Button.test.tsx
+++ src/components/button/Button.test.tsx
@@ -1,31 +1,21 @@
-import React from "react";
-import { fireEvent, render } from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect";
+import { render, fireEvent, screen } from "@testing-library/react";
 import Button from "./Button";
 
-test("numbers", () => {
-  const a = 1;
-  const b = 2;
-
-  const expected = 3;
-
-  const result = a + b;
-
-  expect(result).toBe(expected);
-});
-
-//OKAY,THIS DOESN'T WORK. I NEED IMPROVE AT TESTING :(
-
-/*describe("<Button/>", () => {
+describe("<Button/>", () => {
   const mockHandler = jest.fn();
-  const component = render(<Button onClick={mockHandler}>Click me</Button>);
 
   test("renders content", () => {
-    component.getByText("Click me");
+    render(<Button onClick={mockHandler}>Click me</Button>);
+    const button = screen.getByText("Click me");
+
+    expect(button).toBeInTheDocument();
   });
 
   test("click the button", () => {
-    fireEvent.click(component.getByText("Click me"));
+    render(<Button onClick={mockHandler}>Click me</Button>);
+    const button = screen.getByText("Click me");
+
+    fireEvent.click(button);
     expect(mockHandler).toHaveBeenCalledTimes(1);
   });
-});*/
+});
